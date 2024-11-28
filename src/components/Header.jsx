2a9b51d88d653d2f7cce6d16ab6be2a21514406e
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-11-28 09:02:51 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HiHome, HiStar,HiMagnifyingGlass,HiPlayCircle,HiTv } from "react-icons/hi2";
+import { HiHome, HiStar,HiMagnifyingGlass,HiPlayCircle,HiTv, HiPlusCircle } from "react-icons/hi2";
 import logo from "./../assets/Images/logo.png";
 import Headeritem from "./Headeritem";
 
@@ -7,21 +7,26 @@ function Header() {
   const menu = [
     { name: "HOME", icon: HiHome },
     {name:"SEARCH",icon: HiMagnifyingGlass},
+    { name: "ORIGINALS", icon: HiStar },
+    {name:"WATCHLIST",icon:HiPlusCircle},
     {name:"SERIES" ,icon :HiPlayCircle},
     {name:"MOVIES",icon:HiTv},
-    { name: "ORIGINALS", icon: HiStar },
+   
 
   ];
 
   return (
-    <div>
-      <img src={logo} className="w-[120px] md:w-[115px] object-cover p-2" alt="Logo" />
-      <div className="menu">
-        {menu.map((item, index) => (
-          // Pass the name and icon as props to Headeritem
-          <Headeritem key={index} name={item.name} Icon={item.icon} />
-        ))}
-      </div>
+    <div className="flex item-center justify-between p-5">
+        <div  className="flex gap-8 item-center ">
+        <img src={logo} className="w-[120px] md:w-[115px] object-cover p-2" alt="Logo" />
+      
+      {menu.map((item, index) => (
+        // Pass the name and icon as props to Headeritem
+        <Headeritem key={index} name={item.name} Icon={item.icon} />
+      ))}
+        </div>
+      
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4lTwq_Cb_AwEquAa0B6WNOZCpeZvSiVQq8j7xqw8mJcy-9I4YuIX2lQCPg5w2qaaa42A&usqp=CAU" className="w-[40px] rounded-full "/>
     </div>
   );
 }
--- src/components/Headeritem.jsx
+++ src/components/Headeritem.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function Headeritem({ name, Icon }) {
   return (
-    <div className="header-item flex items-center space-x-2">
-      {Icon && <Icon className="h-6 w-6" />} {/* Render the icon */}
-      <h2>{name}</h2> {/* Render the name */}
+    <div className="header-item flex items-center  gap-2  text-[18px] font-semibold cursor-pointer gap-3 hover:underline
+
+    underline-offset-8
+    ">
+      <Icon />
+      <h2>{name}</h2>
     </div>
   );
 }
