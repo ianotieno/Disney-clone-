271f6bd15f17f7be96ea71b0bb69247fcc215704
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-11-29 08:32:19 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css'
 import Header from './components/Header'
+import Slider from './components/Slider'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
     
     <div className=''>
    <Header/>
+   <Slider/>
     </div>
      
        
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -5,7 +5,7 @@ const api_key="f78657d91cff505289aaece483df7cde"
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=f78657d91cff505289aaece483df7cde
 
-const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?" +api_key )
+const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?api_key=" +api_key )
 
 export default{
     getTrendingVideos
