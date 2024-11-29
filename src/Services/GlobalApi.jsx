89bce6b7d609bb5447584c038a6ea64fe778b553
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-11-29 07:10:49 +0300

    update

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key="f78657d91cff505289aaece483df7cde"
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=f78657d91cff505289aaece483df7cde
+
+const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?" +api_key )
+
+export default{
+    getTrendingVideos
+}
